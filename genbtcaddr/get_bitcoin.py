7a49f586ec293a830d3e7eed20e5646896285763
wodingdong <lvguidong_1@163.com>
2021-04-26 09:22:51 +0800

    增加错误判断

--- genbtcaddr/get_bitcoin.py
+++ genbtcaddr/get_bitcoin.py
@@ -20,18 +20,23 @@ def get_bitcoin_balance(address):
     key_len = len(key_word)
     text = response.text
     index = text.find(key_word)
+    if index < 0:
+        print("******can not find index*******")
+        return 0;
     short = text[index:index+100]
     btc = short.find("BTC")
-    
-    btc_banlance = short[key_len+1:btc-1]
-    
-    return btc_banlance
+    if btc < 0:
+        print("******can not find btc******")
+        return 0;
+    else:
+        btc_banlance = short[key_len+1:btc-1]
+        return btc_banlance
 
 if __name__ == "__main__":
     try:
-        # address = "3JZHLAKwc291dnxjwLyDDfnmQkbTNwC7PX"
+        address = "3JZHLAKwc291dnxjwLyDDfnmQkbTNwC7PX"
         # address = "bc1qcn6xjvfy6uqyqqnzmwxeqqtnk9cmtcpummn8la"
-        address = "bc1qeysmlpayh0pqe4xuv6prllww8549sdy8754eyt6ks5tdnzhv2qwq3nktw6"
+        # address = "36KKMy5yihkjNA4Rc21eA5TXy4wnfCGpj3"
         btc_banlance = get_bitcoin_balance(address)
         
         if is_zero_balance(btc_banlance):
